fcf0f9428a4752b1cd42062dac0b5df768af1032
USER AK <rathomimyristica@yahoo.co.id>
2022-11-08 06:35:34 -0800

    swagger docs added

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 require('express-async-errors');
 
+
 //security packages
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const express = require('express');
 const app = express();
@@ -34,8 +40,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.get('/', (req, res)=>{
-  res.send('jobs api')
+  res.send('<h1>jobs Api</h1><a href="/api-docs">Documentation</a>');
 })
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // extra packages
 
